27a2b56007c3d9e0ad0b7793fc8b082f088f1597
andrehsvictor <andrehsvictor@gmail.com>
2024-11-18 16:30:15 -0300

    feat: Create RetenxException class for custom exception handling with status and error details

---
+++ src/main/java/andrehsvictor/retenx/exception/RetenxException.java
@@ -0,0 +1,26 @@
+package andrehsvictor.retenx.exception;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RetenxException extends RuntimeException {
+
+    private HttpStatus status;
+    private List<String> errors;
+
+    public RetenxException(HttpStatus status, List<String> errors) {
+        this.status = status;
+        this.errors = errors;
+    }
+
+    public RetenxException(HttpStatus status, String error) {
+        this.status = status;
+        this.errors = List.of(error);
+    }
+}
