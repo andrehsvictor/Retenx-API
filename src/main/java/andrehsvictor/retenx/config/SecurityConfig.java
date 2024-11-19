33611654a80a47fce776d8868487239b7ee8313a
andrehsvictor <andrehsvictor@gmail.com>
2024-11-19 09:17:04 -0300

    feat: Enhance SecurityConfig to implement a custom SecurityFilterChain with stateless session management and OAuth2 resource server support

--- src/main/java/andrehsvictor/retenx/config/SecurityConfig.java
+++ src/main/java/andrehsvictor/retenx/config/SecurityConfig.java
@@ -2,7 +2,12 @@ package andrehsvictor.retenx.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -23,5 +28,18 @@ public class SecurityConfig {
             "/api/v1/decks",
             "/api/v1/decks/{id}",
     };
-    
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.authorizeHttpRequests(requests -> requests
+                .requestMatchers(HttpMethod.GET, ALLOWED_PATHS_WITH_GET_METHOD).permitAll()
+                .requestMatchers(HttpMethod.POST, ALLOWED_PATHS_WITH_POST_METHOD).permitAll()
+                .requestMatchers(HttpMethod.PUT, ALLOWED_PATHS_WITH_PUT_METHOD).permitAll()
+                .anyRequest().authenticated());
+        http.oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+        return http.build();
+    }
+
 }
