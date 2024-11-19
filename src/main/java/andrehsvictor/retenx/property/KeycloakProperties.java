32e0507a08fae406c5134977507bf90ca81b69a3
andrehsvictor <andrehsvictor@gmail.com>
2024-11-19 07:48:24 -0300

    feat: Add validation to KeycloakProperties for improved configuration integrity

--- src/main/java/andrehsvictor/retenx/property/KeycloakProperties.java
+++ src/main/java/andrehsvictor/retenx/property/KeycloakProperties.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.annotation.Validated;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
@@ -14,6 +15,7 @@ import lombok.Setter;
 
 @Getter
 @Setter
+@Validated
 @Configuration
 @ConfigurationProperties(prefix = "retenx.keycloak")
 public class KeycloakProperties {
