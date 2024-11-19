cf8f4481aeb3c1cffeb6cb0fdb21d28867acaddb
andrehsvictor <andrehsvictor@gmail.com>
2024-11-19 08:06:02 -0300

    feat: Add updateFromPutUserDto method to UserMapper for partial updates

--- src/main/java/andrehsvictor/retenx/user/UserMapper.java
+++ src/main/java/andrehsvictor/retenx/user/UserMapper.java
@@ -1,10 +1,14 @@
 package andrehsvictor.retenx.user;
 
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.MappingConstants.ComponentModel;
 
 import andrehsvictor.retenx.user.dto.GetMeDto;
 import andrehsvictor.retenx.user.dto.PostUserDto;
+import andrehsvictor.retenx.user.dto.PutUserDto;
 
 @Mapper(componentModel = ComponentModel.SPRING)
 public interface UserMapper {
@@ -12,5 +16,8 @@ public interface UserMapper {
     User toUser(PostUserDto postUserDto, String keycloakId);
 
     GetMeDto toGetMeDto(User user, boolean emailVerified);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    User updateFromPutUserDto(@MappingTarget User user, PutUserDto putUserDto);
 
 }
