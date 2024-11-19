1e771bcf188dfcd208c8d989a60f649e84d9cee8
andrehsvictor <andrehsvictor@gmail.com>
2024-11-19 07:48:38 -0300

    feat: Update findByKeycloakId method to findByExternalId for consistency

--- src/main/java/andrehsvictor/retenx/user/UserRepository.java
+++ src/main/java/andrehsvictor/retenx/user/UserRepository.java
@@ -10,6 +10,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     boolean existsByEmail(String email);
 
-    Optional<User> findByKeycloakId(String keycloakId);
+    Optional<User> findByExternalId(String externalId);
     
 }
