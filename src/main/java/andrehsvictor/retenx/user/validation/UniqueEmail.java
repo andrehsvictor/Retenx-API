11b7ed2e29d3679aaa1e1461d7f0adeaa7f49de2
andrehsvictor <andrehsvictor@gmail.com>
2024-11-19 11:01:15 -0300

    fix: Add annotations for field-level validation in UniqueEmail, UniqueUsername, and UnverifiedEmail classes

--- src/main/java/andrehsvictor/retenx/user/validation/UniqueEmail.java
+++ src/main/java/andrehsvictor/retenx/user/validation/UniqueEmail.java
@@ -1,8 +1,15 @@
 package andrehsvictor.retenx.user.validation;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 import andrehsvictor.retenx.user.validation.validator.UniqueEmailValidator;
 import jakarta.validation.Constraint;
 
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
 @Constraint(validatedBy = UniqueEmailValidator.class)
 public @interface UniqueEmail {
     String message() default "E-mail already exists.";
--- src/main/java/andrehsvictor/retenx/user/validation/UniqueUsername.java
+++ src/main/java/andrehsvictor/retenx/user/validation/UniqueUsername.java
@@ -1,8 +1,15 @@
 package andrehsvictor.retenx.user.validation;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 import andrehsvictor.retenx.user.validation.validator.UniqueUsernameValidator;
 import jakarta.validation.Constraint;
 
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
 @Constraint(validatedBy = UniqueUsernameValidator.class)
 public @interface UniqueUsername {
     String message() default "Username already exists.";
