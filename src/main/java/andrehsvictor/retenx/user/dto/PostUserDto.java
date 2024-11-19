48f488d5e4778c4702ab83bf8f2c14f7c75076c2
andrehsvictor <andrehsvictor@gmail.com>
2024-11-19 08:06:32 -0300

    feat: Add validation annotations for email and password in PostUserDto

--- src/main/java/andrehsvictor/retenx/user/dto/PostUserDto.java
+++ src/main/java/andrehsvictor/retenx/user/dto/PostUserDto.java
@@ -2,6 +2,7 @@ package andrehsvictor.retenx.user.dto;
 
 import andrehsvictor.retenx.user.validation.UniqueEmail;
 import andrehsvictor.retenx.user.validation.UniqueUsername;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
@@ -21,6 +22,7 @@ public class PostUserDto {
     private String lastName;
 
     @UniqueEmail
+    @Email(message = "E-mail must be a valid e-mail address.")
     @NotBlank(message = "E-mail is required.")
     @Size(min = 5, max = 255, message = "E-mail must be between 5 and 255 characters.")
     private String email;
@@ -32,6 +34,7 @@ public class PostUserDto {
 
     @NotBlank(message = "Password is required.")
     @Size(min = 8, max = 50, message = "Password must be between 8 and 50 characters.")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$", message = "Password must contain at least one lowercase letter, one uppercase letter, and one digit.")
     private String password;
 
     @Size(max = 500, message = "Bio must be less than 500 characters.")
