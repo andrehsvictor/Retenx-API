d189b461c04e3c3a3c47282d7cc2eff3e9cbe24b
andrehsvictor <andrehsvictor@gmail.com>
2024-11-22 13:44:13 -0300

    refactor: Remove KeycloakUserCreator interface and integrate its functionality into KeycloakUserMapper

--- src/main/java/andrehsvictor/retenx/keycloak/user/KeycloakUserMapper.java
+++ src/main/java/andrehsvictor/retenx/keycloak/user/KeycloakUserMapper.java
@@ -1,5 +1,7 @@
 package andrehsvictor.retenx.keycloak.user;
 
+import java.util.List;
+
 import org.mapstruct.BeanMapping;
 import org.mapstruct.BeforeMapping;
 import org.mapstruct.Mapper;
@@ -9,7 +11,7 @@ import org.mapstruct.NullValuePropertyMappingStrategy;
 
 import andrehsvictor.retenx.user.User;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", imports = { List.class })
 public interface KeycloakUserMapper {
 
     @BeforeMapping
@@ -23,5 +25,11 @@ public interface KeycloakUserMapper {
     @Mapping(target = "password", ignore = true)
     @Mapping(target = "emailVerified", ignore = true)
     KeycloakUser updateKeycloakUserFromUser(User user, @MappingTarget KeycloakUser keycloakUser);
+
+    @Mapping(target = "emailVerified", constant = "false")
+    @Mapping(target = "enabled", constant = "true")
+    @Mapping(target = "realmRoles", expression = "java(List.of(\"user\"))")
+    @Mapping(target = "requiredActions", expression = "java(List.of(\"VERIFY_EMAIL\"))")
+    KeycloakUser userToKeycloakUser(User user);
 
 }
