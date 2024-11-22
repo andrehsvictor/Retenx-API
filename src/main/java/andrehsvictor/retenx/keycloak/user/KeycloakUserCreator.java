48def806eb92e1ac63692172ddf3ac1ab529af4c
andrehsvictor <andrehsvictor@gmail.com>
2024-11-22 07:48:50 -0300

    fix: Update KeycloakUserCreator to set emailVerified to false during user creation

--- src/main/java/andrehsvictor/retenx/keycloak/user/KeycloakUserCreator.java
+++ src/main/java/andrehsvictor/retenx/keycloak/user/KeycloakUserCreator.java
@@ -10,7 +10,7 @@ import andrehsvictor.retenx.user.User;
 @Mapper(componentModel = "spring", imports = { List.class })
 public interface KeycloakUserCreator {
 
-    @Mapping(target = "emailVerified", constant = "true")
+    @Mapping(target = "emailVerified", constant = "false")
     @Mapping(target = "enabled", constant = "true")
     @Mapping(target = "realmRoles", expression = "java(List.of(\"user\"))")
     @Mapping(target = "requiredActions", expression = "java(List.of(\"VERIFY_EMAIL\"))")
