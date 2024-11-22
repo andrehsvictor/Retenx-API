08537d2eb73e8b0815b11a82eccc41b6ba6978db
andrehsvictor <andrehsvictor@gmail.com>
2024-11-22 15:07:44 -0300

    feat: Enhance DeckRepository with additional query methods for user-specific access and deletion

--- src/main/java/andrehsvictor/retenx/deck/DeckRepository.java
+++ src/main/java/andrehsvictor/retenx/deck/DeckRepository.java
@@ -1,7 +1,22 @@
 package andrehsvictor.retenx.deck;
 
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+
+import jakarta.transaction.Transactional;
 
 public interface DeckRepository extends JpaRepository<Deck, Long> {
+
+    Optional<Deck> findByIdAndUsersUserIdOrVisibility(Long id, Long userId, DeckVisibility visibility);
+
+    Page<Deck> findAllByUsersUserId(Long userId, Pageable pageable);
+
+    @Modifying
+    @Transactional
+    void deleteByIdAndAuthorId(Long id, Long authorId);
 
 }
