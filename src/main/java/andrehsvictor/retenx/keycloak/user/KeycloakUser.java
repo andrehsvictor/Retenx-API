35617aad69bc6a545855d3da294d5e42318d6d47
andrehsvictor <andrehsvictor@gmail.com>
2024-11-22 07:48:39 -0300

    refactor: Replace UserResource with UserRepresentation in KeycloakUser class for improved user data handling

--- src/main/java/andrehsvictor/retenx/keycloak/user/KeycloakUser.java
+++ src/main/java/andrehsvictor/retenx/keycloak/user/KeycloakUser.java
@@ -3,10 +3,8 @@ package andrehsvictor.retenx.keycloak.user;
 import java.io.Serializable;
 import java.util.List;
 
-import org.keycloak.admin.client.resource.UserResource;
 import org.keycloak.representations.idm.CredentialRepresentation;
 import org.keycloak.representations.idm.UserRepresentation;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,117 +18,97 @@ public class KeycloakUser implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    private UserResource userResource;
+    private UserRepresentation userRepresentation = new UserRepresentation();
 
     public String getFirstName() {
-        return userResource.toRepresentation().getFirstName();
+        return userRepresentation.getFirstName();
     }
 
     public String getLastName() {
-        return userResource.toRepresentation().getLastName();
+        return userRepresentation.getLastName();
     }
 
     public String getEmail() {
-        return userResource.toRepresentation().getEmail();
+        return userRepresentation.getEmail();
     }
 
     public String getUsername() {
-        return userResource.toRepresentation().getUsername();
+        return userRepresentation.getUsername();
     }
 
     public String getId() {
-        return userResource.toRepresentation().getId();
+        return userRepresentation.getId();
     }
 
     public List<String> getRealmRoles() {
-        return userResource.toRepresentation().getRealmRoles();
+        return userRepresentation.getRealmRoles();
     }
 
     public List<String> getRequiredActions() {
-        return userResource.toRepresentation().getRequiredActions();
+        return userRepresentation.getRequiredActions();
     }
 
     public boolean isEnabled() {
-        return userResource.toRepresentation().isEnabled();
+        return userRepresentation.isEnabled();
     }
 
     public boolean isEmailVerified() {
-        return userResource.toRepresentation().isEmailVerified();
+        return userRepresentation.isEmailVerified();
     }
 
     public String getAvatarUrl() {
-        return userResource.toRepresentation().firstAttribute("avatarUrl");
+        return userRepresentation.firstAttribute("avatarUrl");
     }
 
     public String getAttribute(String key) {
-        return userResource.toRepresentation().firstAttribute(key);
+        return userRepresentation.firstAttribute(key);
     }
 
     public void setAttribute(String key, String value) {
-        userResource.toRepresentation().singleAttribute(key, value);
+        userRepresentation.singleAttribute(key, value);
     }
 
     public void setFirstName(String firstName) {
-        userResource.toRepresentation().setFirstName(firstName);
+        userRepresentation.setFirstName(firstName);
     }
 
     public void setLastName(String lastName) {
-        userResource.toRepresentation().setLastName(lastName);
+        userRepresentation.setLastName(lastName);
     }
 
     public void setEmail(String email) {
-        userResource.toRepresentation().setEmail(email);
+        userRepresentation.setEmail(email);
     }
 
     public void setUsername(String username) {
-        userResource.toRepresentation().setUsername(username);
+        userRepresentation.setUsername(username);
     }
 
     public void setRealmRoles(List<String> realmRoles) {
-        userResource.toRepresentation().setRealmRoles(realmRoles);
+        userRepresentation.setRealmRoles(realmRoles);
     }
 
     public void setRequiredActions(List<String> requiredActions) {
-        userResource.toRepresentation().setRequiredActions(requiredActions);
+        userRepresentation.setRequiredActions(requiredActions);
     }
 
     public void setEnabled(boolean enabled) {
-        userResource.toRepresentation().setEnabled(enabled);
+        userRepresentation.setEnabled(enabled);
     }
 
     public void setEmailVerified(boolean emailVerified) {
-        userResource.toRepresentation().setEmailVerified(emailVerified);
+        userRepresentation.setEmailVerified(emailVerified);
     }
 
     public void setPassword(String password) {
         CredentialRepresentation credential = new CredentialRepresentation();
         credential.setType(CredentialRepresentation.PASSWORD);
         credential.setValue(password);
-        userResource.toRepresentation().setCredentials(List.of(credential));
+        userRepresentation.setCredentials(List.of(credential));
     }
 
     public void setAvatarUrl(String avatarUrl) {
-        userResource.toRepresentation().singleAttribute("avatarUrl", avatarUrl);
-    }
-
-    public void update() {
-        userResource.update(userResource.toRepresentation());
-    }
-
-    public void delete() {
-        userResource.remove();
-    }
-
-    public void sendVerifyEmail() {
-        userResource.sendVerifyEmail();
-    }
-
-    public void executeActionsEmail(List<String> actions) {
-        userResource.executeActionsEmail(actions);
-    }
-
-    public UserRepresentation toRepresentation() {
-        return userResource.toRepresentation();
+        userRepresentation.singleAttribute("avatarUrl", avatarUrl);
     }
 
 }
