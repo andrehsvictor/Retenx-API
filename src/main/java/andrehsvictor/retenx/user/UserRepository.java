da104ca262baa95c3754ae1f31912486a4d0384c
andrehsvictor <andrehsvictor@gmail.com>
2024-11-23 17:20:33 -0300

    feat: Add findById method to UserRepository for retrieving users by UUID

--- src/main/java/andrehsvictor/retenx/user/UserRepository.java
+++ src/main/java/andrehsvictor/retenx/user/UserRepository.java
@@ -1,9 +1,12 @@
 package andrehsvictor.retenx.user;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findById(UUID id);
 
 }
